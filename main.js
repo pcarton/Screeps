bc491f363d8d849dc29cb6200e5b548f5c4a3bfc
Patrick Carton <pdc21446@uga.edu>
2016-07-21 02:31:55 -0400

    added Upgrader role integration and spawning, refactored code for readibility

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
 var roleHarvester = require('role.harvester');
-var maxHarvesters = 3;
+var roleUpgrader = require('role.upgrader');
 
-var maxCreeps = maxHarvesters;
+var hBody = [WORK, CARRY, MOVE];
+var uBody = [WORK, CARRY, MOVE, TOUGH];
+
+var maxHarvesters = 3;
+var maxUpgraders = 2;
+
+var maxCreeps = maxHarvesters + maxUpgraders;
 
 function getNumHarvesters(){
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  console.log('Harvesters: ' + harvesters.length);
+  var hL = harvesters.length;
+  if(Memory.roles.numHarvesters != hL){
+    Memory.roles.numHarvesters = hL;
+    console.log('Harvesters: ' + hL);
+  }
+  return hL;
+}
 
-  Memory.roles.numHarvesters = harvesters;
+function getNumUpgraders(){
+  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  var uL = upgraders.length;
+  if(Memory.roles.numUpgraders != uL){
+    Memory.roles.numUpgraders = uL;
+    console.log('Upgraders: ' + uL);
+  }
+  return uL;
 }
 
 module.exports.loop = function () {
 
-  getNumHarvesters();
+  var h  = getNumHarvesters();
+  var u = getNumUpgraders();
+  Memory.roles.numCreeps = h + u;
 
   for(var name in Game.creeps) {
       var creep = Game.creeps[name];
       if(creep.memory.role == 'harvester') {
           roleHarvester.run(creep);
       }
+      if(creep.memory.role == 'upgrader') {
+          roleUpgrader.run(creep);
+      }
   }
 
 
-  if(Memory.numCreeps < maxCreeps){
-      console.log("in loop 1");
+  if(Memory.roles.numCreeps < maxCreeps){
       for(var spawn in Game.spawns){
-          console.log("in loop 2");
         if((!Game.spawns[spawn].spawning) && (Game.spawns[spawn].energy>=200)){
-            console.log("in if one");
             if(Memory.roles.numHarvesters < maxHarvesters){
-               var c = Game.spawns[spawn].createCreep([WORK, CARRY, MOVE], undefined,{role: 'harvester'});
-               console.log(c);
+               var c = Game.spawns[spawn].createCreep(hBody, undefined,{role: 'harvester'});
+               console.log("Spawned: " + c);
+            }
+            if(Memory.roles.numUpgraders < maxUpgraders){
+               var c = Game.spawns[spawn].createCreep(uBody, undefined,{role: 'upgrader'});
+               console.log("Spawned: " + c);
             }
         }
       }
   }
-  
+
 }
