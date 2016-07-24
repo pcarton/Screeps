1f429fede26b3247d57cb5456bc1654653601b52
Patrick Carton <pdc21446@uga.edu>
2016-07-24 00:11:46 -0400

    made sure that the storage found was the closest; added mem clearing function

--- module.common.js
+++ module.common.js
@@ -1,18 +1,26 @@
 var modCommon = {
   getEn: function(creep){
 
-    var storage = creep.room.find(FIND_STRUCTURES, {
+    var storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
       filter: (object)=>(object.structureType === STRUCTURE_CONTAINER) && (object.store[RESOURCE_ENERGY] > creep.carryCapacity)
     });
-    if(storage.length){
-      var getEnergy = creep.withdraw(storage[0], RESOURCE_ENERGY, creep.carryCapacity-creep.carry);
+    if(storage!==null){
+      var getEnergy = creep.withdraw(storage, RESOURCE_ENERGY, creep.carryCapacity-creep.carry);
       if(getEnergy===ERR_NOT_IN_RANGE) {
-          creep.moveTo(storage[0]);
+          creep.moveTo(storage);
       }
     }else if(creep.memory.selfHarvest){
       var sources = creep.room.find(FIND_SOURCES);
       if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(sources[0]);
+      }
+    }
+  },
+
+  clearDead: function(){
+    for(var i in Memory.creeps) {
+      if(!Game.creeps[i]) {
+          delete Memory.creeps[i];
       }
     }
   }
