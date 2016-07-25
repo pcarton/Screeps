9f133d2ad8767bb2b67d8045855a27f5d62ee918
Patrick Carton <pdc21446@uga.edu>
2016-07-24 22:04:21 -0400

    added priorities to which containers get filled first
    Currently spawn/extention>towers>containers

--- role.harvester.js
+++ role.harvester.js
@@ -19,17 +19,35 @@ var roleHarvester = {
           }
       }
       else {
-          var targets = creep.room.find(FIND_STRUCTURES, {
+          var p1 = creep.room.find(FIND_STRUCTURES, {
                   filter: (structure) => {
                       return ((structure.structureType == STRUCTURE_EXTENSION ||
-                              structure.structureType == STRUCTURE_SPAWN ||
-                              structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity) || (structure.structureType == STRUCTURE_CONTAINER && (structure.store[RESOURCE_ENERGY]<structure.storeCapacity));
+                              structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity);
                   }
           });
-          if(targets.length > 0) {
-              if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(targets[0]);
+          var p2 = creep.room.find(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                  }
+                });
+          var p3 = creep.room.find(FIND_STRUCTURES,{
+            filter: (structure) => {
+              return (structure.structureType == STRUCTURE_CONTAINER && (structure.store[RESOURCE_ENERGY]<structure.storeCapacity));
+            }
+          });
+
+          if(p1.length > 0) {
+              if(creep.transfer(p1[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(p1[0]);
               }
+          }else if(p2.length>0){
+            if(creep.transfer(p2[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(p2[0]);
+            }
+          }else if(p3.length>0){
+            if(creep.transfer(p3[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(p3[0]);
+            }
           }else{
             //If storage is full, Build to not waste time
             roleBuilder.run(creep);
