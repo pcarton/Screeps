467754e878d53abaf01e537f0b382f7388fb6777
Patrick Carton <pdc21446@uga.edu>
2016-08-16 17:07:35 -0400

    added skeleton of the structures module to control tower, etc

---
+++ module.structures.js
@@ -0,0 +1,21 @@
+var roleStructures = {
+
+  //Tower related functions
+  inSight:function(pos, tower){
+
+  },
+  attack:function(tower){
+
+  },
+
+  heal:function(tower){
+
+  },
+
+  run:function(tower){
+
+  }
+  //End Tower related functions
+};
+
+module.exports = roleStructures;
