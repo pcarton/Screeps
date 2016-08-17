28c8025d5a6e4329688ec7365c2d682db325f5cc
Patrick Carton <pdc21446@uga.edu>
2016-08-17 12:50:19 -0400

    added initial targeting and attacking, currently global targets

--- module.structures.js
+++ module.structures.js
@@ -1,11 +1,21 @@
+//Tower initial Memory
+var initialTowerMem = {
+  "target":null,
+  "mode":"attack"
+};
+
 var roleStructures = {
 
   //Tower related functions
   inSight:function(pos, tower){
 
   },
-  attack:function(tower){
 
+  attack:function(tower){
+    var enemy = Game.getObjectById(Memory.tower.target);
+    if(enemy){
+      tower.attack(enemy);
+    }
   },
 
   heal:function(tower){
@@ -13,7 +23,25 @@ var roleStructures = {
   },
 
   run:function(tower){
+    if(Memory.tower.mode === "attack"){
+      attack(tower);
+    }else{
+      heal(tower);
+    }
 
+  },
+
+  pickTargets:function(controller){
+    //priority Targets
+    var pTargets = controller.pos.findInRange(FIND_HOSTILE_CREEPS,20);
+    if(pTargets.length>0){
+      Memory.tower.target = pTargets[0].id;
+    }else{
+      var targets = controller.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+      if(targets.length>0){
+        Memory.tower.target = targets[0].id;
+      }
+    }
   }
   //End Tower related functions
 };
