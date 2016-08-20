f1017c261352554a8f751ca5f059cc9197167198
Patrick Carton <pdc21446@uga.edu>
2016-08-19 20:09:08 -0400

    made it so upgraders, etc will withdraw from storage as well.

--- module.common.js
+++ module.common.js
@@ -18,7 +18,7 @@ var modCommon = {
     }
 
     var storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-      filter: (object)=>(object.structureType === STRUCTURE_CONTAINER) && (object.store[RESOURCE_ENERGY] > creep.carryCapacity)
+      filter: (object)=>(object.structureType === STRUCTURE_CONTAINER || object.structureType === STRUCTURE_STORAGE) && (object.store[RESOURCE_ENERGY] > creep.carryCapacity)
     });
     if(storage!==null){
       var getEnergy = creep.withdraw(storage, RESOURCE_ENERGY, creep.carryCapacity-creep.carry);
