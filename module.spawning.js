36de816f6a1b26c9b45db6be654e3ccfad91d3fd
Patrick Carton <pdc21446@uga.edu>
2016-08-22 11:09:40 -0400

    changed the booleans again since the moveOn var takes care of the same job

--- module.spawning.js
+++ module.spawning.js
@@ -30,9 +30,9 @@ var modSpawning = {
 
     var noHarvest = (Memory.roles.numHarvesters < 1);
 
-    var spawnTier1 = (betterHarvesters1<1 && (energyCapacity<550 || (spawner.room.energyAvailable<550 && noHarvest))); //boolean for tier 1 spawning
+    var spawnTier1 = (energyCapacity<550 || (spawner.room.energyAvailable<550 && noHarvest)); //boolean for tier 1 spawning
 
-    var spawnTier2 = ((betterHarvesters2<1) && (energyCapacity<800 || (spawner.room.energyAvailable<800 && notEnoughHarvest)));  //boolean for tier2 spawning
+    var spawnTier2 = (energyCapacity<800 || (spawner.room.energyAvailable<800 && notEnoughHarvest));  //boolean for tier2 spawning
 
     var spawnTier3 = (energyCapacity>=800);  //boolean for tier3 spawning
 
