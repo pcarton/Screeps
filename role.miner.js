aab3860f73491c27d888e2f8c0c026482326fec4
Patrick Carton <pdc21446@uga.edu>
2016-08-28 15:48:05 -0400

    added a function to assign a source to run when first spawned

--- role.miner.js
+++ role.miner.js
@@ -1,5 +1,28 @@
 var roleMiner = {
 
+  assignSource:function(creep){
+    var sources = creep.room.find(FIND_SOURCES);
+    for(var sIndex in sources){
+      var s = sources[sIndex];
+      var gID = s.id;
+      var thisAssigned =false;
+      for(var cName in Game.creeps){
+          var c = Game.creeps[cName];
+          if(c.memory.source === gID){
+            thisAssigned = true;
+          }
+      }
+      if(!thisAssigned && creep.memory.source === ""){
+        creep.memory.source = gID;
+      }
+      Memory.roles.maxMiners = sources.length;
+    }
+  },
+
+  run: function(creep) {
+
+  }
+
 };
 
 module.exports = roleMiner;
