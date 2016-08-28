9ce9222ef0501ab3c923452b93a4a1bf550a2da3
Patrick Carton <pdc21446@uga.edu>
2016-08-28 15:48:31 -0400

    added some documentation

--- role.hauler.js
+++ role.hauler.js
@@ -1,3 +1,7 @@
+//moves from containers marked by flags to other storage
+//Flags: Drop-Off: where miners leave resouce
+//Deliver: where upgraders, etc will likely pick up
+//priority from first to last(energy): Spawn/Extensions - Tower - Deliver flags - Storage
 var roleHauler = {
 
 };
