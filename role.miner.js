42df36712bfd44508fd281e06ec755c294acc8ec
Patrick Carton <pdc21446@uga.edu>
2016-08-28 17:25:09 -0400

    added assigning closest reciever as drop off and run functionality

--- role.miner.js
+++ role.miner.js
@@ -1,3 +1,5 @@
+var modCommon = require('module.common');
+
 var roleMiner = {
 
   assignSource:function(creep){
@@ -19,10 +21,57 @@ var roleMiner = {
     }
   },
 
+  assignDropOff:function(creep){
+    if(creep.memory.source === ""){
+      assignSource(creep);
+    }
+    var sourceID = creep.memory.source;
+    var source = null;
+    if(sourceID){
+      source = Game.getObjectById(sourceID);
+    }
+    var dropOff = source.pos.findClosestByPath(FIND_STRUCTURES, (structure) => struture.structureType === "STRUCTURE_CONTAINER" || struture.structureType === "STRUCTURE_STORAGE" || struture.structureType === "STRUCTURE_LINK");
+
+    creep.memory.dropOff = dropOff.id;
+  },
+
   run: function(creep) {
 
-  }
+    if(creep.memory.source === ""){
+      assignSource(creep);
+    }
+    if(creep.memory.dropOff === ""){
+      assignDropOff(creep);
+    }
 
+    var sPos = null;
+    var sourceID = creep.memory.source;
+    var source = null;
+    if(sourceID){
+      source = Game.getObjectById(sourceID);
+      sPos = source.pos;
+    }
+
+    var dPos = null;
+    var dropOffID = creep.memory.dropOff;
+    var dropOff = null;
+    if(dropOffID){
+      dropOff = Game.getObjectById(dropOffID);
+      dPos = dropOff.pos;
+    }
+
+    if(creep.carry.energy == creep.carryCapacity){
+      if(creep.transfer(dropOff, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        modCommon.move(creep,dPos);
+      }else{
+        creep.memory.path = null;
+      }
+    }else{
+      if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        modCommon.move(creep,sPos);
+      }
+    }
+  }
 };
 
 module.exports = roleMiner;
