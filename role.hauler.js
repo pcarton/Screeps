28b5f3e08650e2a588ac410fbefd44bb93d8b024
Patrick Carton <pdc21446@uga.edu>
2016-09-07 20:32:29 -0400

    changed priority of hauler
    Previously Extension/Spawn >Tower >Deliver >Storage
    Now Extension/Spawn >Deliver>Tower>Storage

--- role.hauler.js
+++ role.hauler.js
@@ -37,9 +37,9 @@ var roleHauler = {
     var deliver = null;
     if(p1){
       deliver = p1;
-    }else if(p2){
-      deliver = p2;
     }else if(p3){
+      deliver = p2;
+    }else if(p2){
       deliver = p3;
     }else if(p4){
       deliver = p4;
