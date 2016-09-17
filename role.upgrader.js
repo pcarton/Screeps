aab830a6a5cb571253859dda8afb03b6a431bbd0
Patrick Carton <pdc21446@uga.edu>
2016-09-16 21:02:54 -0400

    made it only possible to conserve during safeMode

--- role.upgrader.js
+++ role.upgrader.js
@@ -6,27 +6,29 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.working && creep.carry.energy === 0) {
-            creep.memory.working = false;
-            creep.memory.path = null;
-        }
-        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.working = true;
-            creep.memory.path = null;
-        }
+      var conserve = Memory.conservation && creep.room.controller.safeMode;
 
-        if(creep.memory.working && !(Memory.conservation && creep.room.controller.ticksToDowngrade>4200)) {
-          var controller = creep.room.controller;
-            if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
-              modCommon.move(creep,controller.pos);
-            }
-        }
-        else if(creep.memory.working){
-          roleHauler.run(creep);
-        }else{
-          modCommon.getEn(creep);
-        }
-    }
+      if(creep.memory.working && creep.carry.energy === 0) {
+          creep.memory.working = false;
+          creep.memory.path = null;
+      }
+      if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+          creep.memory.working = true;
+          creep.memory.path = null;
+      }
+
+      if(creep.memory.working && !(conserve && creep.room.controller.ticksToDowngrade>4200)) {
+        var controller = creep.room.controller;
+          if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+            modCommon.move(creep,controller.pos);
+          }
+      }
+      else if(creep.memory.working){
+        roleHauler.run(creep);
+      }else{
+        modCommon.getEn(creep);
+      }
+  }
 };
 
 module.exports = roleUpgrader;
