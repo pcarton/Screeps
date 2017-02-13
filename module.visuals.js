22127380712d24ec3383ec76be22488d271bdd54
Patrick Carton <pdc21446@uga.edu>
2017-02-13 13:35:35 -0500

    added potential things to visualize

--- module.visuals.js
+++ module.visuals.js
@@ -1,4 +1,9 @@
 //TODO add a UI for the room that has most accessed info
+//Source&Mineral Amount/time to regen
+//Storage.store of non zero resources
+//Room.energyAvailable
+//Currently Spawning
+//Currently Targeted
 var modVisuals = {
 
 };
