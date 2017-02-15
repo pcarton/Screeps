7388a4a60089b8b3adb03c8664c45b58227f588d
Patrick Carton <pdc21446@uga.edu>
2017-02-15 17:44:42 -0500

    added cost map

--- module.bodyTypes.js
+++ module.bodyTypes.js
@@ -28,6 +28,17 @@ var merchantBody = [MOVE,CARRY];
 
 //TODO Higher tier(s) for lvl 6,7,8
 
+var costMap = {
+  MOVE:50,
+  WORK:100,
+  CARRY:50,
+  ATTACK:80,
+  RANGED_ATTACK:150,
+  HEAL:250,
+  CLAIM:600,
+  TOUGH:10
+};
+
 //Object that holds a getter for these body types
 var bodyObj = {
 
