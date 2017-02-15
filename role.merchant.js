5284236dd56fce6566aa61cf9ae43406689b296b
Patrick Carton <pdc21446@uga.edu>
2017-02-15 17:04:54 -0500

    added getting OrderId again if cant get object

--- role.merchant.js
+++ role.merchant.js
@@ -143,7 +143,9 @@ var roleMerchant = {
 
     //TODO keep track of toLoad and put that much in the terminal
     //If toTrade.amount == 0, calculate energy needed from terminal and fill with that
-    }
+  }else{
+    getOrder(creep);
+  }
     creep.memory.toLoad.amount = toLoad;
   }
 
