84b16bae96beb400c33e814827a95d063cd7185f
Patrick Carton <pdc21446@uga.edu>
2017-02-21 09:00:53 -0500

    Added initial file for new module to help when expanding

---
+++ module.assist.js
@@ -0,0 +1,7 @@
+//TODO room assist module: have one room send creeps to work in a new
+//room and recycle themselves at end of life to jump start the room
+var modAssist = {
+
+};
+
+module.exports = modAssist;
