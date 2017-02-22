59a8d9041ccee171e020c11a49f4b2d385f4f2a0
Patrick Carton <pdc21446@uga.edu>
2017-02-22 10:57:45 -0500

    Added notes and TODOs

--- module.memory.js
+++ module.memory.js
@@ -48,7 +48,11 @@ var defaultTower = {
 
 var modMemory = {
 
-  init:function(){}, //TODO init whole structure with global info
+  init:function(){
+    for (var room in Game.rooms){
+      
+    }
+  }, //TODO init whole structure with global info
   initRoom:function(roomName){}, //TODO init room with all room info
   initCreep:function(creep, creepType){}, //TODO init creep with data currently down in modSpawning
   initTower:function(tower){}, //TODO init tower with individual aspects (currently global)
