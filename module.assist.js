ca3b11a6122fa024a756a9baac2b78ee1dd66725
Patrick Carton <pcarton927@gmail.com>
2017-03-08 15:01:35 -0500

    implemented assist functionality

--- module.assist.js
+++ module.assist.js
@@ -3,13 +3,21 @@
 var modConstants = require('module.constants');
 var modCommon = require('module.common');
 
+var controllerPos = Game.rooms[creep.memory.roomName].controller.pos;
+
 var modAssist = {
 
   run:function(creep){
-    if(creep.ticksToLive <= modConstants.nearDeath){
-      //TODO near death, recycle
-    }else if(creep.memory.roomName === creep.room.name){
-      //TODO in right room
+    if(creep.memory.roomName !== creep.room.name){
+      modCommon.move(creep,controllerPos);
+    }else if(creep.ticksToLive <= modConstants.nearDeath){
+      var spawn =creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType === STRUCTURE_SPAWN);
+        }
+      });
+      if(spawn.recycleCreep(creep) === ERR_NOT_IN_RANGE)
+        modCommon.move(creep,spawn.pos);
     }else{
       modCommon.runCreep(creep);
     }
