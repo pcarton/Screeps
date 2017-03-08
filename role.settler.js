b84b6c29d18af87e379e8205494783fd891db5bb
Patrick Carton <pcarton927@gmail.com>
2017-03-08 15:51:22 -0500

    initial implementation of settler run function

--- role.settler.js
+++ role.settler.js
@@ -1,5 +1,25 @@
 var roleSettler = {
 
+  run: function(creep){
+    var goto = creep.memory.dest;
+    //If not in the right room yet
+    if(goto && creep.pos != goto){
+      modCommon.move(creep,goto);
+    }else if(goto){ //if at the rally
+      goto = null;
+    }else{ //if in right room and arrived at rally
+      var contoller = creep.room.controller;
+      var success = creep.claimController(controller);
+      if(success === ERR_NOT_IN_RANGE){
+        modCommon.move(creep, controller.pos);
+      }else if(success === OK){
+        creep.memory.role = upgrader;
+        creep.memory.selfHarvest = true;
+      }else{
+        creep.suicide(); //TODO think of something better to do
+      }
+    }
+  }
 };
 
 module.exports = roleSettler;
