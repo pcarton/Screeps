4ceeae9f8f2c5986c022352fa1ae6e92a7a5289e
Patrick Carton <pcarton927@gmail.com>
2017-03-09 20:02:41 -0500

    moved defaults into the methods to prevent(?) permutations in the future

--- module.memory.js
+++ module.memory.js
@@ -1,53 +1,3 @@
-var defaultCreep = {
-  role:"none",
-  path:null,
-  dest:null,
-  assist:"", //set to roomName of assisted room
-  room:"",
-};
-var defaultRoom = {
-  roles: {
-      numCreeps:0,
-      numHarvesters:0,
-      maxHarvesters:1,
-      numUpgraders:0,
-      maxUpgraders:1,
-      numBuilders:0,
-      maxBuilders:1,
-      numRepair:0,
-      maxRepair:1,
-      numArchitects:0,
-      betterHarvesters1:0,
-      betterHarvesters2:0,
-      numMiners:0,
-      maxMiners:0,
-      numHaulers:0,
-      maxHaulers:1,
-      numFeeders:0,
-      maxFeeders:1,
-      numGeo:0,
-      maxGeo:1,
-      numGeoH:0,
-      maxGeoH:0,
-      numMerchant:0,
-      maxMerchant:0,
-    },
-  spawnQ:[],
-  conservation: false,
-  fortify: false,
-  towers:[],
-  sourceIDs:[],
-  signMsg:"",
-  //TODO extend the creep prototype and add signing based on this memory value
-  //http://support.screeps.com/hc/en-us/articles/203013212-Creep#signController
-  initialized:false, //Set to true when init is run
-
-};
-var defaultTower = {
-  target:null,
-  mode:"attack"
-};
-
 var modMemory = {
 
   init:function(){
@@ -58,21 +8,67 @@ var modMemory = {
     Memory.initialized = true;
   },
   initRoom:function(roomName){
-    Memory.rooms[roomName] = defaultRoom;
+    Memory.rooms[roomName] = {
+      roles: {
+          numCreeps:0,
+          numHarvesters:0,
+          maxHarvesters:1,
+          numUpgraders:0,
+          maxUpgraders:1,
+          numBuilders:0,
+          maxBuilders:1,
+          numRepair:0,
+          maxRepair:1,
+          numArchitects:0,
+          betterHarvesters1:0,
+          betterHarvesters2:0,
+          numMiners:0,
+          maxMiners:0,
+          numHaulers:0,
+          maxHaulers:1,
+          numFeeders:0,
+          maxFeeders:1,
+          numGeo:0,
+          maxGeo:1,
+          numGeoH:0,
+          maxGeoH:0,
+          numMerchant:0,
+          maxMerchant:0,
+        },
+      spawnQ:[],
+      conservation: false,
+      fortify: false,
+      towers:[],
+      sourceIDs:[],
+      signMsg:"",
+      //TODO extend the creep prototype and add signing based on this memory value
+      //http://support.screeps.com/hc/en-us/articles/203013212-Creep#signController
+      initialized:false, //Set to true when init is run
+
+    };
     //TODO any other things go here (like Tower init)
     Memory.rooms[roomName].initialized = true;
   },
   initTower:function(tower){
     var roomName = tower.room.name;
     var towerID = tower.id;
-    Memory.rooms[roomName].towers[tower.id] = defaultTower;
+    Memory.rooms[roomName].towers[tower.id] = {
+      target:null,
+      mode:"attack"
+    };
   },
   getSpawnQ:function(roomName){
     return Memory.rooms[roomName].spawnQ;
   },
   changeCreepNum:function(roomName,creepType,num,set){}, //creepType - creepJob to change, num, num to add or set to, set - bool, true to set, false to add
   getInitalCreepMem(creepType){
-    var memoryObj = defaultCreep;
+    var memoryObj ={
+      role:"none",
+      path:null,
+      dest:null,
+      assist:"", //set to roomName of assisted room
+      room:"",
+    };
     if(creepType === "harvester"){
       memoryObj.role = 'harvester';
       memoryObj.selfHarvest = true;
