786c61f5f629dd9f9cf23e44dd94744e1cbbf3c8
Patrick Carton <pcarton927@gmail.com>
2017-05-31 19:35:41 -0400

    moved some of the bool calculation to inside if

--- module.common.js
+++ module.common.js
@@ -77,22 +77,23 @@ var modCommon = {
 
   stillToFix:function(object){
     var modifier = Math.max(Math.pow(10,controlLvl-3),10);
-    //Priority
-    var brokenRoad = object.structureType ===STRUCTURE_ROAD && (object.hits < 3000);
-    var brokenRamp = object.structureType ===STRUCTURE_RAMPART && (object.hits < 5000)&& (object.hitsMax-object.hits>0);
-    var brokenCont = object.structureType ===STRUCTURE_CONTAINER && (object.hits < 100000);
-    var brokenWall = object.structureType ===STRUCTURE_WALL && (object.hits < 5000) && (object.hitsMax-object.hits>0);
-    //Priority 2
-    var brokenRamp2 = object.structureType ===STRUCTURE_RAMPART && (object.hits < (500*modifier+modConstants.structBuffer))&& (object.hitsMax-object.hits>0);
-    //Lowest Priority
-    var brokenWall2 = object.structureType ===STRUCTURE_WALL && (object.hits < (500*modifier+modConstants.structBuffer)) && (object.hitsMax-object.hits>0);
 
     if(room.storage.store.energy < modConstants.roomEnergyBuffer){
+      var brokenRoad = object.structureType ===STRUCTURE_ROAD && (object.hits < 3000);
+      var brokenRamp = object.structureType ===STRUCTURE_RAMPART && (object.hits < 5000)&& (object.hitsMax-object.hits>0);
+      var brokenCont = object.structureType ===STRUCTURE_CONTAINER && (object.hits < 100000);
+      var brokenWall = object.structureType ===STRUCTURE_WALL && (object.hits < 5000) && (object.hitsMax-object.hits>0);
+
       return brokenRoad || brokenRamp || brokenCont || brokenWall;
-    }else if(brokenRamp2){
-      return brokenRamp2;
-    }else {
-      return brokenWall2;
+    }else{
+      var brokenRamp2 = object.structureType ===STRUCTURE_RAMPART && (object.hits < (500*modifier+modConstants.structBuffer))&& (object.hitsMax-object.hits>0);
+
+      var brokenWall2 = object.structureType ===STRUCTURE_WALL && (object.hits < (500*modifier+modConstants.structBuffer)) && (object.hitsMax-object.hits>0);
+      if(brokenRamp2){
+        return brokenRamp2;
+      }else {
+        return brokenWall2;
+      }
     }
     return false;
   },
