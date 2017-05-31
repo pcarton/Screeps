96f07dff552347533614486b9bc11bb0f405eff1
Patrick Carton <pcarton927@gmail.com>
2017-05-31 14:13:40 -0400

    fixed typo

--- module.memory.js
+++ module.memory.js
@@ -134,7 +134,7 @@ var modMemory = {
     for(i = 0; i<50; i++){
       for(j = 0; j<50; j++){
         if(i===0 || i===49 || j===0 || j===49){
-          roomBorders.push(RoomPosition(i,j,roomName));
+          roomBorders.push(new RoomPosition(i,j,roomName));
         }
       }
     }
