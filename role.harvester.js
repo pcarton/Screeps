0036afffea3812a447613ebbe43550bbf0e2e918
Patrick Carton <pdc21446@uga.edu>
2017-06-20 02:47:47 -0400

    added more null checks

--- role.harvester.js
+++ role.harvester.js
@@ -84,8 +84,14 @@ var roleHarvester = {
               return (flag.name.substring(0,9)==="Container" || flag.name.substring(0,7)==="DropOff" || flag.name.substring(0,7)==="Deliver");
             }
           });
-          var p4Arr = creep.room.lookForAt(LOOK_STRUCTURES, p4Flag);
-          var p4 = _.filter(p4Arr, (object) => object.structureType == STRUCTURE_CONTAINER)[0];
+          var p4 = null;
+          if(p4Flag){
+            var p4Arr = creep.room.lookForAt(LOOK_STRUCTURES, p4Flag);
+            if(p4Arr){
+              p4 = _.filter(p4Arr, (object) => object.structureType == STRUCTURE_CONTAINER)[0];
+            }
+          }
+
           if(p1) {
               if(creep.transfer(p1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 modCommon.move(creep,p1.pos);
