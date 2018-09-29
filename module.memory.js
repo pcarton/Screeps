57ce69710be577daad4620eac01bedb3eaf63d0b
Patrick Carton <pcarton927@gmail.com>
2018-09-29 18:08:21 -0400

    added initial memory module

---
+++ module.memory.js
@@ -0,0 +1,28 @@
+var modMemory = {
+
+  init(){
+    Memory.rooms = {};
+    for(var roomName in Game.rooms){
+      this.initRoom(roomName);
+    }
+    Memory.initialized = true;
+  },
+
+  initRoom(roomName){
+    Memory.rooms[roomName] = {
+      spawnQ: [],
+      jobQ: [],
+      sourceIDs:[],
+      signMsg:"",
+      initialized:false, //Set to true when init is run
+    };
+    Memory.rooms[roomName].initialized = true;
+  },
+
+  storeJob(creep, job){
+    creep.memory.job = job
+  }
+
+};
+
+module.exports = modMemory;
