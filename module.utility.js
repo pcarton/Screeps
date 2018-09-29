845d44529552c3ec0a4498a71add92f6412cb5c5
Patrick Carton <pcarton927@gmail.com>
2018-09-29 15:50:49 -0400

    added parts check in the run function

--- module.utility.js
+++ module.utility.js
@@ -3,13 +3,17 @@ var modUtil = {
   runCreep: function(creep){
     var job = creep.memory.job;
     if(job){
-      if(creep.pos.inRangeTo(job.target,job.range){
-        job.action(creep, target)
-      }else{
-        creep.moveTo(target)
-      }
-      if(job.endCondition(creep)){
+      if(!job.partsRequired.every(val => creep.body.includes(val))){
         creep.memory.job = null
+      }else{
+        if(creep.pos.inRangeTo(job.target,job.range){
+          job.action(creep, target)
+        }else{
+          creep.moveTo(target)
+        }
+        if(job.endCondition(creep)){
+          creep.memory.job = null
+        }
       }
     }
    
