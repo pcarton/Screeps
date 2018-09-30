ae2ebe2dd93fb0ee32477953413e91455caef84d
Patrick Carton <pcarton927@gmail.com>
2018-09-30 11:19:13 -0400

    added room logic

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+var spawnModule = require("./module.spawning.js");
 //main loop
 module.exports.loop = function () {
 
-    //assign the right run method to each creep based on its role
-    for(var name in Game.creeps) {
-        creep = Game.creeps[name];
-        //TODO
+    for (const roomName in Game.rooms){
+        spawnModule.spawn(roomName);
+        
+        const allCreeps = Game.creeps;
+        const thisRoomCreeps = _.filter(allCreeps, creep =>{
+            creep.room.name === roomName;
+        });
+
+        if(thisRoomCreeps < 5){
+            spawnModule.enqueueGeneric();
+        }
+
+        for(var name in Game.creeps) {
+            creep = Game.creeps[name];
+            //TODO
+        }
     }
 
 };
--- module.spawning.js
+++ module.spawning.js
@@ -1,66 +1,24 @@
+var memoryModule = require("./module.memory.js");
+
 var modSpawning = {
 
-    tierOneGenericBody = [],
+    tierOneGenericBody = [WORK,MOVE,MOVE,CARRY,CARRY],
 
     enqueueGeneric(roomName, tier){
-      if(!tier){
-        tier = this.calcTier(roomName);
-      }
-        
       var genericCreep = {
         description:"Generic Creep",
-        body: null,
+        body: this.tierOneGenericBody,
         name: undefined,
         memory:{
             job: null,
         }
       };
-      harvesterObj.body = bodyObj.getBody('harvester',tier);
-      //Using unshift() to add to front, basic priority
-      //Only use this for miners/harvesters
-      Memory.rooms[roomName].spawnQ.unshift(harvesterObj);
-      modUtil.incrementCreepNum("harvester",roomName);
-    },
-  
-    //Returns a number between 1 and highest tier inclusive
-    calcTier:function(roomName){
-      var energyCapacity = Game.rooms[roomName].energyCapacityAvailable;
-      var notEnoughHarvest = (Memory.rooms[roomName].roles.numHarvesters <= 0) && (Memory.rooms[roomName].roles.numMiners <= 0);
-  
-      var noFeeders = Memory.rooms[roomName].roles.numFeeders === 0;
-      var noMiners = Memory.rooms[roomName].roles.numMiners === 0;
-      var noHaulers = Memory.rooms[roomName].roles.numHaulers === 0;
-  
-      var spawnTier1 = (energyCapacity<modConstants.tier2EnergyMin);
-      var spawnTier2 = (energyCapacity<modConstants.tier3EnergyMin);
-      var spawnTier3 = (energyCapacity<modConstants.tier4EnergyMin || !Game.rooms[roomName].storage || (Game.rooms[roomName].storage.store.energy < modConstants.tier3To4Buffer && (noFeeders && noMiners && noHaulers)));
-      var spawnTier4 = (energyCapacity<modConstants.tier5EnergyMin);
-      var spawnTier5 = (energyCapacity<modConstants.tier6EnergyMin);
-      var spawnTier6 = (energyCapacity<modConstants.tier7EnergyMin);
-      var spawnTier7 = (energyCapacity>=modConstants.tier7EnergyMin);
-  
-      if(spawnTier1 || notEnoughHarvest){
-        return 1;
-      }else if(spawnTier2){
-        return 2;
-      }else if(spawnTier3){
-        return 3;
-      }else if(spawnTier4){
-        return 4;
-      }else if(spawnTier5){
-        return 5;
-      }else if(spawnTier6){
-        return 6;
-      }else if(spawnTier7){
-        return 7;
-      }
-      return 1;
+      memoryModule.enqueueCreep(roomName,genericCreep);
     },
   
     //dequeue and spawn
     spawn:function(roomName){
-      var available = Game.rooms[roomName].energyAvailable;
-      var queue = Memory.rooms[roomName].spawnQ;
+      var queue = memoryModule.getSpawnQueue(roomName);
       if(queue.length > 0){
         var spawners = Game.rooms[roomName].find(FIND_STRUCTURES, {
           filter: (structure) => {
@@ -78,10 +36,6 @@ var modSpawning = {
             console.log("Room "+roomName+": Spawning "+toSpawn.description+": "+creep + "\n" + JSON.stringify(toSpawn.body));
           }else{
               queue.unshift(toSpawn);
-              //console.log("Could not spawn", toSpawn.description, "ERROR:",spawn.canCreateCreep(toSpawn.body,toSpawn.name));
-              if(queue.length == Memory.rooms[roomName].roles.numCreeps && !spawn.spawning){
-                this.enqueueHarvester(roomName,1); //FIXME change to have logic for if there are miners but no haulers, etc
-              }
           }
         }
       }
