50574ee5840e61052cbeec5bb643a2f09267f6d3
Patrick Carton <patrick.d.carton@gmail.com>
2025-01-27 20:44:33 -0500

    fix upgrade container haul logic

--- overlord.js
+++ overlord.js
@@ -95,11 +95,11 @@ var overlord = {
         var activeHaulTasks = task.getAssignedTasksOfType(roomName,"haul");
         var controllerSouroundings = room.lookAtArea(controller.pos.y - 3, controller.pos.x - 3, controller.pos.y + 3, controller.pos.x + 3, true);
         var controllerContainers = _.filter(controllerSouroundings, function (object) {
-            return (object.type = "structure" && object.structure && object.structure.structureType == STRUCTURE_CONTAINER);
+            return (object.type == "structure" && object.structure && object.structure.structureType == STRUCTURE_CONTAINER);
         });
         var controllerContainer = null;
         if (controllerContainers && controllerContainers.length > 0) {
-            controllerContainer = controllerContainers[0];
+            controllerContainer = Game.getObjectById(controllerContainers[0].structure.id);
         }
         var isActiveSpawnDuty = false;
         var isActiveUgradeHauler = false;
